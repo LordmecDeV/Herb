daad167e353ef1fefc9b1a3c67098a233782b2f3
Johnny Almeida <johnny.almeida@zarpo.com.br>
2022-12-07 21:00:32 -0300

    alterando forma de login do herb

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,8 @@ class User extends Authenticatable
         'email',
         'password',
         'cargo',
+        'provider', 
+        'provider_id'
     ];
 
     /**
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -165,7 +165,8 @@ iframe {
   padding: 15px;
   position: relative;
   background-color: #0093E9; 
-  background-image: linear-gradient(160deg, #0093E9 0%, #80D0C7 100%);
+  background: white;
+  
 }
 
 .wrap-login100 {
@@ -201,7 +202,7 @@ iframe {
   width: 100%;
   top: 0;
   left: 0;
-  background-color: #4F4F4F;
+  background-color: #4285F4;
   padding-top: 50px;
   padding-bottom: 39px;
 }
@@ -287,7 +288,7 @@ iframe {
   padding: 0 20px;
   width: 100%;
   height: 50px;
-  background-color: #00BFFF;
+  background-color: #4285F4;
   border-radius: 25px;
 
   font-family: 'Nunito', sans-serif;
@@ -3384,20 +3385,12 @@ background-image: linear-gradient(160deg, #4F4F4F 0%, #4F4F4F 100%);
 					<a ><img src="https://i.ibb.co/305TYbS/logo-zarpo-320x132-transparente.png" style="max-width: 100%;height: 60px;display: block; margin-left:auto;margin-right:auto;" alt="logo-zarpo-320x132-transparente" border="0"></a>
 					</span>
 
-					<div class="wrap-input100 validate-input m-b-16" data-validate="Please enter username">
-						<input class="input100" type="text" name="email" required autofocus placeholder="master@zarpo.com.br">
-                        @if ($errors->has('email'))
-                                      <span class="text-danger">{{ $errors->first('email') }}</span>
-                        @endif
-						<span class="focus-input100"></span>
+					<div class="wrap-input100 validate-input m-b-16">
+					<a href="{{ url('/login/google') }}"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2048px-Google_%22G%22_Logo.svg.png" style="max-width: 100%;height: 200px;display: block; margin-left:auto;margin-right:auto;" alt="logo-zarpo-320x132-transparente" border="0"></a>
 					</div>
 
-					<div class="wrap-input100 validate-input" data-validate = "Please enter password">
-						<input class="input100" type="password" name="password" required placeholder="******">
-                        @if ($errors->has('password'))
-                                      <span class="text-danger">{{ $errors->first('password') }}</span>
-                                  @endif
-						<span class="focus-input100"></span>
+					<div class="wrap-input100 validate-input">
+						
 					</div>
 
 					<div class="text-right p-t-13 p-b-23">
@@ -3410,20 +3403,15 @@ background-image: linear-gradient(160deg, #4F4F4F 0%, #4F4F4F 100%);
 						</a>
 					</div>
 
-					<div class="container-login100-form-btn">
-						<button class="login100-form-btn" type="submit">
-							Entrar
+						<div class="container-login100-form-btn">
+						<button class="login100-form-btn" style="margin-bottom:30px;" type="button">
+						<a href="{{ url('/login/google') }}" style="color:white;text-decoration:none;font-size:16px;">
+    					{{ __('Entrar com Google') }}
+						</a><svg style="margin-left:10px;" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
+  <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"/>
+</svg>
 						</button>
 					</div>
-
-					<div class="flex-col-c p-t-17 p-b-40">
-						<span class="txt1 p-b-9">
-							Ainda não tem uma conta?
-						</span>
-
-						<a href="/registration" class="txt3">
-							Registrar-se
-						</a>
 					</div>
 				</form>
 			</div>
